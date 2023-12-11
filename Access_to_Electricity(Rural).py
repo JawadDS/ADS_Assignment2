844164f32ab365048e4edf47483688d84f9e62fd
Jawad Iqbal <147210456+JawadDS@users.noreply.github.com>
2023-12-11 23:42:46 +0000

    Add files via upload

--- Access_to_Electricity(Rural).py
+++ Access_to_Electricity(Rural).py
@@ -1,27 +1,36 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec 11 20:21:46 2023
-
 @author: Jay
 
-The below code read a csv file and explore some different statistical tools such as describe, groupby, transpose, cumsum and prodsum.
+The below code reads a CSV file and explores various statistical tools such as describe, groupby, transpose, cumsum, and cumprod.
 
-The data has been taken from the world bank website 
-link = https://api.worldbank.org/v2/en/indicator/EG.ELC.ACCS.RU.ZS?downloadformat=csv
-The Data was too large so i just took the data of 9 countries for 4 years and save it in a file ('Access_to_Electricity')
-and i have upload that file to my github repositry, so in order to run this code you need use the file from my github ripostry.
-Link = https://github.com/JawadDS/ADS_Assignment2/blob/main/Access_to_Electricity(Rural).csv
+The data has been extracted from the World Bank website using the following link:
+https://api.worldbank.org/v2/en/indicator/EG.ELC.ACCS.RU.ZS?downloadformat=csv
 
+Due to the large size of the data, a subset containing information for 9 countries over 4 years has been saved to a file ('Access_to_Electricity').
+This file is hosted on my GitHub repository, and to run this code, you can access it from the following link:
+https://github.com/JawadDS/ADS_Assignment2/blob/main/Access_to_Electricity(Rural).csv
 """
 
 import pandas as pd
 
-# Load the data from the CSV file
-file_path = 'Access_to_Electricity(Rural).csv'
+def read_data_function(filename):
+    """
+    Reads the data from a CSV file into a pandas DataFrame.
+
+    Parameters:
+    - filename (str): The path to the CSV file.
+
+    Returns:
+    - data (DataFrame): The loaded data.
+    """
+    # Read the data from the CSV file into a pandas dataframe
+    data = pd.read_csv(filename, index_col=0)
+    return data
 
 # Read CSV data into a pandas DataFrame with 'Country' as the index
-electricity_data = pd.read_csv(file_path, index_col=0)
-
+electricity_data = read_data_function('Access_to_Electricity(Rural).csv')
 # Transpose the DataFrame to have 'Year' as the column index
 transposed_data = electricity_data.transpose()
 
--- Electric_Power_Consumption.py
+++ Electric_Power_Consumption.py
@@ -1,25 +1,38 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Dec 10 18:53:13 2023
-
 @author: Jay
 
-The below code produce a bar plot of the data  Electic Power Consumption for 9 countries
+The below code produces a bar plot of Electric Power Consumption data for 9 countries
 for the period of 2011 to 2014.
 
-The data has been taken from the world bank website 
-link = https://data.worldbank.org/indicator/EG.USE.ELEC.KH.PC The Data was too large so i just took the data of 9 countries for 4 years and save it in a file ('Access_to_Electricity')
-and i have upload that file to my github repositry, so in order to run this code you need use the file from my github ripostry.
-Link = https://github.com/JawadDS/ADS_Assignment2/blob/main/Electric_Power_Consumption.csv
-
+The data has been obtained from the World Bank website: 
+https://data.worldbank.org/indicator/EG.USE.ELEC.KH.PC 
+As the original dataset was too large, a subset containing information for 9 countries over 4 years 
+was saved to a file ('Electric_Power_Consumption.csv'). To run this code, use the file from my GitHub repository.
+Link: https://github.com/JawadDS/ADS_Assignment2/blob/main/Electric_Power_Consumption.csv
 """
 
 # Import necessary libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Read the data from the CSV file into a pandas dataframe
-data = pd.read_csv('Electric_Power_Consumption.csv')
+def read_data_function(filename):
+    """
+    Reads the data from a CSV file into a pandas DataFrame.
+
+    Parameters:
+    - filename (str): The path to the CSV file.
+
+    Returns:
+    - data (DataFrame): The loaded data.
+    """
+    # Read the data from the CSV file into a pandas dataframe
+    data = pd.read_csv(filename)
+    return data
+
+# Read CSV data into a pandas DataFrame
+data = read_data_function('Electric_Power_Consumption.csv')
 
 # Set 'Country' as the index for better visualization
 data.set_index('Country', inplace=True)
@@ -31,12 +44,12 @@ data.plot(kind='bar', alpha=0.7)
 # Setting labels and title for better understanding
 plt.xlabel('Countries')
 plt.ylabel('kWh')
-plt.title('Electric power consumption (kWh per capita)')
+plt.title('Electric Power Consumption (kWh per capita)')
 
 # Place the legend outside the plot area for better visibility
-plt.legend(title='Years' ,  loc='upper left', bbox_to_anchor=(1, 1))
+plt.legend(title='Years', loc='upper left', bbox_to_anchor=(1, 1))
 
-# Set the y-axis range from 0 to 100 for a percentage scale
+# Set the y-axis range from 0 to 5000 for better scaling
 plt.ylim(0, 5000)
 
 # Display the plot
