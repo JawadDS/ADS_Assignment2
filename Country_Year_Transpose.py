a6f47d17ab054be29a1e68233078d101b1e9b6d0
Jawad Iqbal <147210456+JawadDS@users.noreply.github.com>
2023-12-13 00:54:22 +0000

    Add files via upload

--- Country_Year_Transpose.py
+++ Country_Year_Transpose.py
@@ -4,29 +4,28 @@ Created on Sun Dec 10 18:53:13 2023
 
 @author: Jay
 
-The below code is the answare for question 1 in the assignmnet, In this code
-I have made a function which takes a file and returen 2 Dataframes one with countries as 
-columns and one with years as column.
+The below code is the answer for question 1 in the assignment. In this code,
+I have created a function that takes a file and returns two DataFrames, 
+one with countries as columns and one with years as columns.
 
-The data has been taken from the world bank website 
-link = https://api.worldbank.org/v2/en/indicator/EG.ELC.ACCS.ZS?downloadformat=csv
-The Data was too large so i just took the data of 9 countries for 4 years and save it in a file ('Access_to_Electricity')
-and i have upload that file to my github repositry, so in order to run this code you need use the file from my github ripostry.
-Link = https://github.com/JawadDS/ADS_Assignment2/blob/main/Access_to_Electricity.csv
-
-
+The data has been retrieved from the World Bank website.
+Link: https://api.worldbank.org/v2/en/indicator/EG.ELC.ACCS.ZS?downloadformat=csv
+The data was too large, so I selected data for 9 countries for 4 years and saved it in a file ('Access_to_Electricity').
+I have uploaded that file to my GitHub repository, so to run this code, you need to use the file from my GitHub repository.
+Link: https://github.com/JawadDS/ADS_Assignment2/blob/main/Access_to_Electricity.csv
 
 """
 
 import pandas as pd
+import numpy as np
 
 def read_data_function(data_file):
     """
     Reads a CSV file containing data on access to electricity.
-    
+
     Parameters:
         data_file (str): The path to the CSV file.
-        
+
     Returns:
         df_countries (DataFrame): Original DataFrame with countries as rows.
         df_years (DataFrame): Transposed DataFrame with years as rows.
@@ -42,8 +41,32 @@ def read_data_function(data_file):
     
     return df_countries, df_years
 
-# Calling the function and saving the returend DataFrames
-df_countries, df_years = read_data_function('Access_to_Electricity.csv')
+def skew(df_countries):
+    """ Calculates the centralized and normalized skewness of distribution. """
+    
+    # calculates average and std, dev for centralizing and normalizing
+    aver = np.mean(df_countries)
+    std = np.std(df_countries)
+    
+    # now calculate the skewness
+    value = np.sum(((df_countries-aver) / std)**3) / len(df_countries-2)
+    
+    return value
+
+def kurtosis(df_countries):
+    """ Calculates the centralized and normalized excess kurtosis of distribution. """
+    
+    # calculates average and std, dev for centralizing and normalizing
+    aver = np.mean(df_countries)
+    std = np.std(df_countries)
+    
+    # now calculate the kurtosis
+    value = np.sum(((df_countries-aver) / std)**4) / len(df_countries-3) - 3.0
+    
+    return value
+
+# Calling the function and saving the returned DataFrames
+df_countries, df_years = read_data_function('Access_to_Electricity(Rural).csv')
 
 # Display the original DataFrame with countries as rows
 print("DataFrame with Countries as Rows:")
@@ -52,3 +75,19 @@ print(df_countries)
 # Display the transposed DataFrame with years as rows
 print("\nDataFrame with Years as Rows:")
 print(df_years)
+
+# Additional statistical calculations
+print("Skewness:")
+print(skew(df_countries))
+
+print("\nKurtosis:")
+print(kurtosis(df_countries))
+
+print("\nDescriptive Statistics:")
+print(df_countries.describe())
+
+print("\nCumulative Sum:")
+print(df_countries.cumsum())
+
+print("\nCumulative Product:")
+print(df_countries.cumprod())
